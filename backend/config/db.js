c041a5936a3c448b9da88e179b1a9c291629344a
rahul <rahul.d@encycg.com>
2025-08-20 17:35:50 +0530

    fix the db connection issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,13 +7,13 @@ const connectDB = async () => {
     const uri =
       process.env.NODE_ENV === "production"
         ? process.env.MONGO_URI_PROD
-        : process.env.MONGO_URI_DEV;
+        : process.env.MONGO_URI_PROD;
 
     await mongoose.connect(uri, {
       serverSelectionTimeoutMS: 10000,
     });
 
-    console.log(`MongoDB Connected: ${mongoose.connection.host}`);
+    console.log(`MongoDB Connected`);
   } catch (error) {
     console.error("DB connection failed:", error.message);
     process.exit(1);
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ dotenv.config();
 
 // DB Connection
 connectDB();
-
 const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
