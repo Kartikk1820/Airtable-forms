9fbce1924bdf36e7e03bbacdb3d1449f4ecf598e
rahul <rahul.d@encycg.com>
2025-08-20 18:32:26 +0530

    fix render err

--- frontend/airtable-forms-frontend/vite.config.js
+++ frontend/airtable-forms-frontend/vite.config.js
@@ -1,21 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import dotenv from "dotenv";
 
-dotenv.config();
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": process.env.VITE_API_URL,
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api":
+          env.VITE_API_URL || "https://airtable-forms-backend.onrender.com",
+      },
     },
-  },
-  preview: {
-    host: true,
-    port: process.env.PORT ? Number(process.env.PORT) : 4173,
-    allowedHosts: ["airtable-forms.onrender.com"],
-  },
+    preview: {
+      host: true,
+      port: process.env.PORT ? Number(process.env.PORT) : 4173,
+      allowedHosts: ["airtable-forms.onrender.com"],
+    },
+  };
 });
